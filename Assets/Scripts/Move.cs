e09dfb515e4b44eb19a4ba099af0b0d00bcb6daa
Rachel Kim <rachelgekim@gmail.com>
2024-06-18 11:30:49 -0400

    Update complete move script

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,18 +4,52 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    [SerializeField] private float _velocity = 1.5f;
+    public float speed;
+    public float jumpPower;
+
+    private int _jumpCounter = 0;
+    private float _horizontal;
+    private bool _isFacingRight = true;
 
     private Rigidbody2D _rb;
 
-    // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        _horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && _jumpCounter > 0)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, jumpPower);
+            _jumpCounter--;
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector2(_horizontal * speed, _rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        if (_isFacingRight && _horizontal < 0f || !_isFacingRight && _horizontal > 0f)
+        {
+            _isFacingRight = !_isFacingRight;
+            GetComponent<SpriteRenderer>().flipX = _isFacingRight;
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        GameManager.instance.GameOver();
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _jumpCounter = 2;
+        }
     }
 }
