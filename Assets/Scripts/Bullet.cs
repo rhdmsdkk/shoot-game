e60f897946f432187b8906d4c44767f52a7d8ec8
Rachel Kim <rachelgekim@gmail.com>
2024-06-20 11:28:12 -0400

    Add bullet and enemy scripts

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 20f;
+    public int damage = 20;
     public Rigidbody2D rb;
+    public GameObject hitEffect;
 
     void Start()
     {
         rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Enemy enemy = collision.GetComponent<Enemy>();
+        if (enemy)
+        {
+            enemy.takeDamage(20);
+        }
+
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Ground"))
+        {
+            Instantiate(hitEffect, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
     }
 }
