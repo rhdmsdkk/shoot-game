92cd733e2e3171d2a6618b0081716220164861cc
Rachel Kim <rachelgekim@gmail.com>
2024-06-20 11:46:04 -0400

    Add death animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public int health = 100;
+    public GameObject deathEffect;
 
     public void takeDamage(int damage)
     {
@@ -18,6 +19,7 @@ public class Enemy : MonoBehaviour
 
     public void Die()
     {
+        Instantiate(deathEffect, transform.position, transform.rotation);
         Destroy(gameObject);
     }
 }
