5b20117bdfdb666a3e290891e3934e073db1c336
Rachel Kim <rachelgekim@gmail.com>
2024-06-19 23:44:16 -0400

    Add ability to fire bullets

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bullet;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, firePoint.position, firePoint.rotation);
+    }
+}
