eb64fcc98502fb4353ba7162a49ae337b8cdac91
Rachel Kim <rachelgekim@gmail.com>
2024-06-26 16:08:10 -0400

    Add fire rate to guns

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,8 +7,26 @@ public class Weapon : MonoBehaviour
     public Transform firePoint;
     public GameObject bullet;
 
+    public float fireRate = 0.5f;
+
+    private float lastShotTime;
+    private bool canShoot = true;
+
+    private void Update()
+    {
+        if (Time.time >= lastShotTime + fireRate)
+        {
+            canShoot = true;
+        }
+    }
+
     public void Shoot()
     {
-        Instantiate(bullet, firePoint.position, firePoint.rotation);
+        if (canShoot)
+        {
+            Instantiate(bullet, firePoint.position, firePoint.rotation);
+            lastShotTime = Time.time;
+        }
+        canShoot = false;
     }
 }
